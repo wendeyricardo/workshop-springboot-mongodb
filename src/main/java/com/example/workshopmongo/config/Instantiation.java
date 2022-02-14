bd09fc7ac5e63ed1148cc9411c443d549e556470
Wendey Ricardo <wendeyricardo@gmail.com>
2022-02-13 22:56:19 -0300

    referenciando os posts do usuarios

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -13,6 +13,7 @@ import com.example.workshopmongo.repository.UserRepository;
 import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
 import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.dto.AuthorDTO;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -36,10 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("11/02/2002"), "bom dia", "hoje acordei feliz", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("11/02/2002"), "bom dia", "hoje acordei feliz", new AuthorDTO (maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
