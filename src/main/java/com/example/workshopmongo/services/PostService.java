536bde67755a1807e2270f2001612ab2d6a7c60d
Wendey Ricardo <wendeyricardo@gmail.com>
2022-02-16 23:40:47 -0300

    consulta simples com query method

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -23,5 +23,9 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 
 }
